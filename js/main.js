22e151eb68876db3799c17f776646776446dfcba
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2022-01-28 21:46:31 +0800

    реализовали модальное окно

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* ==================== SWIPER  ====================*/
+
 const swiper = new Swiper(".review__slider", {
   slidesPerView: 3,
   spaceBetween: 70,
@@ -27,4 +29,22 @@ const swiper = new Swiper(".review__slider", {
       spaceBetween: 70,
     },
   },
+});
+
+/* ==================== MODAL  ====================*/
+const modalWindow = document.querySelector(".modal");
+const buttonModal = document.querySelector(".display__button");
+const body = document.querySelector("body");
+
+buttonModal.addEventListener("click", () => {
+  modalWindow.classList.add("active");
+  body.classList.add("lock");
+});
+
+modalWindow.addEventListener("click", (e) => {
+  const isModal = e.target.closest(".modal__inner");
+  if (!isModal) {
+    modalWindow.classList.remove("active");
+    body.classList.remove("lock");
+  }
 });
